8ad165ee530307ce9cb1e2346cecd071e411ede2
mulandi <mulandimorris1@gmail.com>
2025-05-19 08:50:23 +0300

    dvpkg

--- main.py
+++ main.py
@@ -10,10 +10,9 @@ def generate_problem():
     operator = random.choice(OPERATORS)
 
     expr = str(left) + " " + operator + " " + str(right)
-    print(expr)
 
     answer = eval(expr)
     return expr, answer
 
 expr, answer = generate_problem()
-print(expr, answer)
+print(expr,"=", answer)
